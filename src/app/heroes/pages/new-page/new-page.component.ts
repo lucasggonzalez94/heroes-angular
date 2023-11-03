d7839bd2bf3a127d3f7bfb1133187fdf9c82cb96
lucasggonzalez94 <lucasggonzalez94@outlook.com>
2023-11-03 14:25:35 -0300

    html de nuevo heroe

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  public publishers = [
+    {
+      id: 'DC Comics',
+      value: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      value: 'Marvel - Comics'
+    }
+  ];
+
 }
