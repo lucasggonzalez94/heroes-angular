bb7618fe726a72171cd2e9081b451ffcd6720b92
lucasggonzalez94 <lucasggonzalez94@outlook.com>
2023-11-06 19:22:54 -0300

    proteccion de rutas publicas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 import { AuthGuard } from './auth/guards/auth.guard';
+import { PublicGuard } from './auth/guards/public.guard';
 
 const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ),
+    canActivate: [ PublicGuard ],
+    canMatch: [ PublicGuard ]
   },
   {
     path: 'heroes',
